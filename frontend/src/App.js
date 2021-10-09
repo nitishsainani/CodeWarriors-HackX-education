6360d651295a78ac93443ca5076112bc10dfcf41
farrukh.ehsan <tusharkaramchandani25@gmail.com>
2021-10-09 16:52:46 +0530

    adding code structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
-
-import './App.css';
-import Navigation from './components/navigation';
-import Home from './Layouts/home';
+import "./App.css";
+import Navigation from "./components/navigation";
+import Home from "./Layouts/home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <Home/>
+      <Router>
+        <Switch>
+          <Navigation />
+          <Home />
+        </Switch>
+      </Router>
     </div>
   );
 }
