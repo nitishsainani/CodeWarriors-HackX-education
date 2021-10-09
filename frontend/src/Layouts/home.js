06beb9b3af39f9e3f876bc1e7ccaf842d2cead03
vignesh-cloud-prog <vigneshun80@gmail.com>
2021-10-09 16:04:33 +0530

    basic structure

---
+++ frontend/src/Layouts/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Introduction from '../components/introduction'
+
+export default function Home() {
+    return (
+        <div>
+            <Introduction/>
+        </div>
+    )
+}
