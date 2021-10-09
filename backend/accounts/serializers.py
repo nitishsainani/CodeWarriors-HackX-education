1ec7d1de13b6f5406de08a83e3e36486e2e35ba8
rizadanish <rizadanish.as@gmail.com>
2021-10-09 07:48:31 -0400

    configured rest auth

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,8 +1,15 @@
-from rest_framework.serializers import ModelSerializer
-from models import User
+from rest_framework import serializers 
+from rest_auth.registration.serializers import RegisterSerializer
+from .models import User
 
 
-class UserSerializer(ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name', 'last_name', 'email', 'is_company', 'is_candidate', ]
+
+class CustomRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    is_company = serializers.BooleanField()
+    is_candidate = serializers.BooleanField()
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #rest-auth
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
+
+urlpatterns= urlpatterns + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns= urlpatterns + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
